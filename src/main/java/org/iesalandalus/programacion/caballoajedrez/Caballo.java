11f7f251e4b671fb337b20a587250fa606258388
Youness <Youness@DESKTOP-BQ87NOV>
2018-11-25 18:09:39 +0100

    Crear la clase Caballo con sus atributos

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.caballoajedrez;
+
+/**
+ *
+ * @author Youness
+ */
+public class Caballo 
+{
+    private Color color;
+    private Posicion posicion;
+    
+}
