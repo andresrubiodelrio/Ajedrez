905299a267f6b989f6fb6f24e89e3a672e2fbdc7
Youness <Youness@DESKTOP-BQ87NOV>
2018-11-25 12:19:39 +0100

    Crear clase enum Direccion con valores que se piden

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Direccion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.caballoajedrez;
+
+/**
+ *
+ * @author Youness
+ */
+public enum Direccion 
+{
+    ARRIBA_IZQUIERDA, ARRIBA_DERECHA, DERECHA_ARRIBA, DERECHA_ABAJO, ABAJO_DERECHA, ABAJO_IZQUIERDA, IZQUIERDA_ARRIBA, IZQUIERDA_ABAJO;
+    
+}
