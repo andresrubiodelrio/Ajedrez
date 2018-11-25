d317ea352edebef37a58dc7ce6fa2a801c321c7b
Youness <Youness@DESKTOP-BQ87NOV>
2018-11-25 12:53:57 +0100

    Insertar getters y setters para los atributos fila y columna

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -13,5 +13,27 @@ public class Posicion
 {
     int fila;
     char columna;
+
+    public int getFila() {
+        return fila;
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setFila(int fila) {
+        if(fila<1 || fila>8)
+            throw new IllegalArgumentException("La fila pasada como parametro no es valida, prueba con este intervalo [1,8]");
+        else
+        this.fila = fila;
+    }
+
+    public void setColumna(char columna) {
+        if(columna<'a' || columna>'h')
+            throw new IllegalArgumentException("La fila pasada como parametro no es valida, prueba con este intervalo [a,h]");
+        else
+        this.columna = columna;
+    }
     
 }
